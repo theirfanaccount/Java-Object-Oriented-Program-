73cbb271178c06aa1144ad0da010f356520d5f5d
MD IRFAN RAZA <irfanraza9626@gmail.com>
2026-08-24 17:26:04 +0530

    completed program

--- Encapsulation/bank.java
+++ Encapsulation/bank.java
@@ -1,6 +1,5 @@
 package Encapsulation;
-
-public class bank {
+class myBank{
     private String name;
     private int accNo;
     private double balance;
@@ -23,5 +22,18 @@ public class bank {
     public double getBalance() {
         return balance;
     }
+}
+
     
+    
+public class bank {
+    public static void main(String[] args){
+        myBank b = new myBank();
+        b.setName("Irfan");
+        b.setAccNo(123456);
+        b.setBalance(1000.50);
+        System.out.println("Account Holder: " + b.getName());
+        System.out.println("Account Number: " + b.getAccNo());
+        System.out.println("Account Balance: $" + b.getBalance());
+    }
 }
