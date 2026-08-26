70f88a684aa46c2757feaf9add7cd7a26b5e16fb
MD IRFAN RAZA <macirfan0216@gmail.com>
2026-08-26 07:10:14 +0530

    static 1st program

--- Static/staticBlock.java
+++ Static/staticBlock.java
@@ -1,7 +1,28 @@
 package Static;
-
+class staticAccess{
+    int a; // instance variable
+    static int b; //static variable
+    //instace block
+    {
+        a = 10;
+        
+    }
+    static {
+        b = 20;
+    }
+    {
+        System.out.println(a); 
+        System.out.println(b);  // can be accessable a static variable inside instance block
+    }
+    //static block
+    static{
+        // System.out.println(a);   can not accessable an instance variable inside static block
+        System.out.println(b);
+    }
+}
 public class staticBlock {
     public static void main(String[] args){
-       System.out.println("hii");
+        staticAccess sa = new staticAccess();
+
     }
 }
