614bb34bdd999cd4d7875b6c4fb13ccd12058785
MD IRFAN RAZA <macirfan0216@gmail.com>
2026-08-28 00:33:21 +0530

    types of inheritanc

---
+++ Inheritance/typesOfInheritance.java
@@ -0,0 +1,27 @@
+package Inheritance;
+// HYBRID INHERITANCE: Mix of Single + Multilevel + Hierarchical
+// Super(Base) class
+class parent{
+
+}
+// -----single inheritance-----
+class child extends parent{
+
+}
+// ------multilevel inheritance-------
+class grandChild extends child{
+
+}
+//------hierachical inheritance--------
+class child1 extends parent{
+
+}
+class child2 extends parent{
+
+}
+//------main class------
+public class typesOfInheritance {
+    public static void main(String[] args){
+        System.out.println("hjdsf");
+    }
+}
