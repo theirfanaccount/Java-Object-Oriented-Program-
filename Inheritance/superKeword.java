8c040cd22279b71e1bcd615c20a6597fefc33d58
MD IRFAN RAZA <macirfan0216@gmail.com>
2026-09-01 15:38:50 +0530

    superKeyword used

---
+++ Inheritance/superKeword.java
@@ -0,0 +1,24 @@
+package Inheritance;
+class first{
+    int i = 123;
+    void method1(){
+        System.out.println("Method of parent/superclass class");
+    }
+}
+class second extends first{
+    int i = 100;//new variable of class second but variable name is same as parent class
+    @Override
+    void method1(){
+        System.out.println(i);
+        System.out.println(super.i);
+        super.method1();
+
+    }
+}
+public class superKeword {
+    public static void main(String[] args){
+        second s = new second();
+        
+        s.method1();
+    }
+}
